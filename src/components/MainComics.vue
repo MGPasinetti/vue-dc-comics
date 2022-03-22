91c405b839bdc722df1b0e9df9bd983014ba8f2a
MGPasinetti <mg.pasinetti@gmail.com>
2022-03-22 17:29:58 +0100

    main done

---
+++ src/components/MainComics.vue
@@ -0,0 +1,33 @@
+<template>
+  <main>
+      <div class="container">
+          {{msg}}
+      </div>
+  </main>
+</template>
+
+<script>
+export default {
+    name: `MainComics`,
+    data () {
+        return {
+            msg: `--> Content goes here <--`,
+        }
+    },
+}
+</script>
+
+<style scoped lang="scss">
+main {
+    background-color: black;
+
+    .container {
+        height: 120px;
+        color: white;
+        display: flex;
+        align-items: center;
+        font-size: 25px;
+        font-weight: 600;
+    }
+}
+</style>
