d231e7b62c801bc3326f40ff3f0e658479d83841
MGPasinetti <mg.pasinetti@gmail.com>
2022-03-22 16:23:45 +0100

    working on style

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,26 @@
 <template>
   <div id="app">
     <HeaderComics />
+    <MainComics />
+    <FooterComics />
   </div>
 </template>
 
 <script>
 import HeaderComics from "./components/HeaderComics.vue"
+import MainComics from "./components/MainComics.vue"
+import FooterComics from "./components/FooterComics.vue"
 
 export default {
   name: 'App',
   components: {
     HeaderComics,
+    MainComics,
+    FooterComics
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "./assets/styles/style.scss";
 </style>
--- src/components/HeaderComics.vue
+++ src/components/HeaderComics.vue
@@ -1,6 +1,13 @@
 <template>
   <header>
-      ciao sono l'header
+      <div class="container">
+        <img src="../assets/img/dc-logo.png" alt="">
+        <nav>
+            <ul>
+                <li v-for="(link, index) in links" :key="index"><a :href="link.href">{{link.text}}</a></li>
+            </ul>
+        </nav>
+      </div>
   </header>
 </template>
 
@@ -9,12 +16,60 @@ export default {
     name: `HeaderComics`,
     data () {
         return {
-
+            links: [
+                {
+                    href: `#`,
+                    text: `Characters`
+                },
+                {
+                    href: `#`,
+                    text: `Comics`
+                },
+                {
+                    href: `#`,
+                    text: `Movies`
+                },
+                {
+                    href: `#`,
+                    text: `Tv`
+                },
+                {
+                    href: `#`,
+                    text: `Games`
+                },
+                {
+                    href: `#`,
+                    text: `Collectibles`
+                },
+                {
+                    href: `#`,
+                    text: `Videos`
+                },
+                {
+                    href: `#`,
+                    text: `Fans`
+                },
+                {
+                    href: `#`,
+                    text: `News`
+                },
+                {
+                    href: `#`,
+                    text: `Shop`
+                }
+            ],
         }
     },
 }
 </script>
 
-<style>
-
+<style scoped lang="scss">
+li {
+    display: inline-block;
+    a {
+        display: inline-block;
+        text-decoration: none;
+        color: black;
+    }
+}
 </style>
