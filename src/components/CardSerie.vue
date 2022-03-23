08c4263b9f9fdf6e497a4c75a20760c8d49ebeac
MGPasinetti <mg.pasinetti@gmail.com>
2022-03-23 16:44:45 +0100

    working on card components

---
+++ src/components/CardSerie.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="card-serie">
+      <img :src="serieThumb" :alt="serieName">
+      <h2>{{ serieName }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+    name: `CardSerie`,
+    props: {
+        serieThumb: String,
+        serieName: String,
+    }
+}
+</script>
+
+<style>
+
+</style>
